d9a8d81565141f97414e0a4a58644f550a65c6e4
Sanchez <hubabuba627@gmail.com>
2019-04-24 20:57:46 +0300

    added material fonts, improve view

--- client/src/components/posts/PostsItem.jsx
+++ client/src/components/posts/PostsItem.jsx
@@ -1,36 +1,42 @@
 import React from 'react';
-import { List, Image, Container, Header, Icon, Button, Divider } from 'semantic-ui-react';
+import { List, Image, Container, Header, Icon, Button, Divider, Card, Segment } from 'semantic-ui-react';
 
 const PostItem = (props) => (
   <Container text>
-    <List>
-      <List.Item>
-        <Image avatar src={props.avatar} />
-        <List.Content>
-          <List.Header>{props.name}</List.Header>
-          {props.date}
-        </List.Content>
-      </List.Item>
-      <Divider clearing />
-      <List.Item>
-        <Header as='h2'>{props.header}</Header>
-      </List.Item>
-      <Divider hidden />
-      <List.Item>
-        <Container text>
-          <p>{props.text}</p>
-        </Container>
-      </List.Item>
-      <Divider hidden />
-      <List.Item>
-        <Button animated>
-          <Button.Content visible>Read</Button.Content>
-          <Button.Content hidden>
-            <Icon name='arrow right' />
-          </Button.Content>
-        </Button>
-      </List.Item>
-    </List>
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Pacifico|Righteous');
+    @import url('https://fonts.googleapis.com/css?family=Simonetta');
+  </style>
+    <Segment compact>
+      <List>
+        <List.Item>
+          <Image avatar src={props.avatar} />
+          <List.Content>
+            <List.Header>{props.name}</List.Header>
+            {props.date}
+          </List.Content>
+        </List.Item>
+        <Divider clearing />
+        <List.Item>
+          <Header as='h2' style={{fontFamily: 'Righteous'}}>{props.header}</Header>
+        </List.Item>
+        <Divider hidden />
+        <List.Item>
+          <Container text style={{fontFamily: 'Simonetta'}}>
+            <p>{props.text}</p>
+          </Container>
+        </List.Item>
+        <Divider hidden />
+        <List.Item>
+          <Button animated>
+            <Button.Content visible>Read</Button.Content>
+            <Button.Content hidden>
+              <Icon name='arrow right' />
+            </Button.Content>
+          </Button>
+        </List.Item>
+      </List>
+    </Segment>  
   </Container>
 )
 
